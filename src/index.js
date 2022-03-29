c58e2349b2e35801dfcba672471074332b12d927
shailesz <sailesh.karki@sagarmatha.edu.np>
2022-03-29 13:44:46 +0545

    used hashrouter

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import Home from "./Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import { NotificationsProvider } from "@mantine/notifications";
 
 ReactDOM.render(
   <React.StrictMode>
     <NotificationsProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />} />
           <Route path="home" element={<Home />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </NotificationsProvider>
   </React.StrictMode>,
   document.getElementById("root")
